a11a0d9be4e8f691bb697b5536f60eae9ed97ebd
Ethan Canton <cethan988@gmail.com>
2024-12-18 14:38:59 -0500

    Day-2 Solution

--- Day-2/main.go
+++ Day-2/main.go
@@ -1,25 +1,82 @@
 package main
 
 import (
-    "bufio"
-    "os"
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-    // Your solution code will go here
+	lines, err := readInput("Day-2/input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	count := countSafeNumbers(lines)
+	println("Number of safe sequences:", count)
+}
+
+func countSafeNumbers(lines []string) int {
+	safeCount := 0
+
+	for _, line := range lines {
+		numbers := strings.Fields(line)
+		prevNum := -1
+		isSafe := true
+		isIncreasing := true  // Track direction
+		directionSet := false 
+
+		for i, numStr := range numbers {
+			num, _ := strconv.Atoi(numStr)
+
+			if i > 0 {
+				diff := num - prevNum
+				// Set direction based on first pair
+				if !directionSet {
+					isIncreasing = diff > 0
+					directionSet = true
+				} else {
+					// Check if direction remains consistent
+					if (diff > 0) != isIncreasing {
+						isSafe = false
+					}
+				}
+				
+				// Check if absolute difference is between 1-3
+				if abs(diff) < 1 || abs(diff) > 3 {
+					isSafe = false
+				}
+			}
+			prevNum = num
+		}
+
+		if isSafe {
+			safeCount++
+		}
+	}
+
+	return safeCount
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
 }
 
 func readInput(filename string) ([]string, error) {
-    file, err := os.Open(filename)
-    if err != nil {
-        return nil, err
-    }
-    defer file.Close()
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
 
-    var lines []string
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        lines = append(lines, scanner.Text())
-    }
-    return lines, scanner.Err()
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
 }
